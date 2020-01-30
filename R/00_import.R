2e3b5edb88a92d75f05aafa5ae204b6165ebee11
gongcastro <gonzalo.garciadecastro@upf.edu>
2020-01-30 21:39:51 +0100

    Adapt to new data

--- R/00_import.R
+++ R/00_import.R
@@ -5,129 +5,63 @@
 #### set up ############################################
 
 # load packages
-library(here)         # for locating files
-library(osfr)         # for connecting to Open Science Framework
+library(osfr)         # for connecting to OSF
 library(magrittr)     # for using pipes
 library(tibble)       # for tidy data presentation
 library(dplyr)        # for manipulating data
 library(tidyr)        # for rehsaping datasets
+library(data.table)   # for importing data
 library(readxl)       # for importing Excel files
 library(lubridate)    # for working with dates
 library(stringr)      # for working with character strings
 library(purrr)        # for working with lists
-library(ggplot2)      # for visualising data
+library(ggplot2)      # for using cut_interval
 library(googledrive)  # for downloading participant-level information
-library(eyetrackingR) # for processing eye-tracking data
+library(here)         # for locating files
 
 # load functions
-source("Code/osf_download_component.R")
-source('Code/target_coords.R')
-source('Code/distractor_coords.R')
-source('Code/prime_coords.R')
+source(here("R", "Functions", "osf_download_folder.R"))
 "%!in%" <- function(x, y) !(x %in% y)
 
 # retrieve data from OSF (takes around a minute, you can download it manually)
 osf_auth("")
-osf_download_component(project = "wekda", component = "Data")
+osf_download_folder(project = "wekda", component = "Data", folder = "Gaze data", local.path = here("Data", "Gaze data"))
 
 # set experimental parameters
-time_bin_duration <- 50  # how long should time bins be in ms?
-sampling_rate     <- 120
-screenX           <- 1920
-screenY           <- 1080
+sampling_rate <- 120  # how many samples does the eye-tracker take per second?
+screenX       <- 1920 # width of the screen in pixels 
+screenY       <- 1080 # height of the screen in pixels
 
 #### import data #######################################
 
 # import participant-level data
-participants <- read_xlsx(here::here("Data/Participant data", "data_participants.xlsx")) %>%
-  mutate(list = paste0("bcn_", language, list))
-
-# import trial-level data
-trials <- as.list(c('Stimuli/list_bcn_cat1.xlsx',
-                    'Stimuli/list_bcn_cat2.xlsx',
-                    'Stimuli/list_bcn_cat3.xlsx',
-                    'Stimuli/list_bcn_spa1.xlsx',
-                    'Stimuli/list_bcn_spa2.xlsx',
-                    'Stimuli/list_bcn_spa3.xlsx')) %>%
-  map(~read_xlsx(.)) %>%
-  set_names(c('bcn_cat1', 'bcn_cat2', "bcn_cat3", "bcn_spa1", "bcn_spa2", "bcn_spa3")) %>%
-  bind_rows(., .id = 'list') %>%
-  mutate(
-    trialID   = as.character(trialID),
-    height_px = 500,
-    width_px  = 500,
-    height_mm = 800,
-    width_mm  = 800
-  )
+participants <- read_xlsx(here("Data", "Participant data", "data_participants.xlsx")) %>% filter(!Pilot) # take info from participants (exclude pilot)
 
 # import eye-tracking data
-data.raw <- map(.x = list.files('Data/Gaze data/Barcelona', full.names = TRUE, recursive = TRUE),
-                ~read.table(.x, sep = ',', dec = '.',
-                            header = TRUE, stringsAsFactors = FALSE)) %>%
-  map(~mutate(., trialType = case_when(trialType == 1 ~ "cognate",
-                                       trialType == 2 ~ "noncognate",
-                                       trialType == 3 ~ "unrelated",
-                                       TRUE           ~ as.character(trialType)))) %>%
-  bind_rows() %>%
-  as_tibble() %>%
-  rename(time  = SystemTimeStamp,
-         ID    = participant,
-         Trial = trialID
-         ) %>%
-  mutate(
-    Trial       = as.character(Trial),
-    ID          = paste0("cognatepriming", ID), 
-    meanX       = meanX*screenX,  # change relative coords to screen coords
-    meanY       = meanY*screenY,  # change relative coords to screen coords
-    lPupil      = ifelse(lPupilV == 0, NA, lPupil), # if non-valid, make NA
-    rPupil      = ifelse(rPupilV == 0, NA, rPupil), # if non-valid, make NA
-    dateTest    = as.Date(dateTest),
-    dateBirth   = as.Date(dateBirth),
-    age         = dateTest-dateBirth,
-    list        = paste0("bcn_", language, list) 
-  ) %>%
-  filter(phase %in% c("prime", "target_distractor")) %>%
-  group_by(ID, Trial, phase) %>% 
-  mutate(
-    time = as.numeric(
-      cut_interval(
-        0:(length(time)-1),
-        length = sampling_rate/1000, # transform time to miliseconds 
-        labels = FALSE)
-    )
-  ) %>%
-  ungroup()
-
-#### process data ###########################################################
-data.processed <- data.raw %>%
-  mutate(
-    Trial = as.character(Trial),
-    # evaluate if gaze is in prime AOI
-    gazeP = prime_coords(data = .,
-                         x_gaze = meanX,
-                         y_gaze = meanY),
-    # evaluate if gaze is in target AOI
-    gazeT = target_coords(data = .,
-                          x_gaze = meanX,
-                          y_gaze = meanY,
-                          target_location = locationTarget),
-    # evaluate if gaze is in distractor AOI
-    gazeD = distractor_coords(data = .,
-                          x_gaze = meanX,
-                          y_gaze = meanY,
-                          target_location = locationTarget),
-  ) %>%
-  select(ID, Trial, phase, time, lPupil, rPupil,
-         meanX, meanY, meanValidity,
-         list, locationTarget, stimulus,
-         gazeP, gazeT, gazeD) %>%
-  left_join(., participants, by = c("ID", "list")) %>% # add participant-level information
-  left_join(., trials, by = c("list", "Trial" = "trialID")) %>% # add trial-level information
-  mutate(meanValidity = meanValidity < 1, # TRUE is non valid
-         distance1 = 650, # distance of right eye from screen (for extracting fixations)
-         distance2 = 650) # distance of left eye from screen (for extracting fixations)
+data <- list.files(here("Data", "Gaze data", "Barcelona"), full.names = TRUE, recursive = TRUE, ) %>%          # list files in the folder
+	map(~fread(., sep = ',', dec = '.', header = TRUE, stringsAsFactors = FALSE)) %>%                          # import files in the folder
+	set_names(str_remove(list.files(here("Data", "Gaze data", "Barcelona")), ".csv")) %>%                      # name each list element with its file name
+	map(~select(., one_of("SystemTimeStamp", "lX", "lY", "lV", "rV", "rX", "rY", "lUserCoordZ", "rUserCoordZ", # select only relevant variables
+						  "Participant", "TrialNum", "Trial", "Phase"))) %>%
+	bind_rows() %>% # merge all datasets
+	as_tibble() %>% # for more informative data-sets
+	mutate(
+		ParticipantID = Participant,
+		TrialID       = as.character(Trial),
+		meanX         = rowMeans(cbind(lX, rX), na.rm = TRUE)*screenX,  # change relative coords to screen coords
+		meanY         = rowMeans(cbind(lY, rY), na.rm = TRUE)*screenY,  # change relative coords to screen coords
+		meanDistance  = rowMeans(cbind(lUserCoordZ, rUserCoordZ), na.rm = TRUE)/10, # change distance from screen to centimeters
+		Trackloss     = !(rV | lV ) # is the sample not valid in both eyes?
+	) %>%
+	drop_na(ParticipantID) %>%
+	group_by(ParticipantID, TrialID, Phase) %>%
+	mutate(TimeStamp = seq(from = 1000/sampling_rate, to = (1000/sampling_rate)*length(SystemTimeStamp), by = 1000/sampling_rate)) %>%
+	ungroup() %>%
+	mutate(ParticipantID = paste0("cognatepriming", as.character(Participant))) %>%
+	filter(ParticipantID %in% participants$ID,
+		   Phase %in% c("Prime", "Target-Distractor")) %>%
+	select(ParticipantID, TrialID, Phase, TimeStamp, meanX, meanY, meanDistance, Trackloss)
 
 #### export data ###############################################
-write.table(data.raw, file = here::here("Data", "00_raw.txt"), sep = "\t", row.names = FALSE)
-write.table(data.processed, file = here::here("Data", "01_processed.txt"), sep = "\t", row.names = FALSE)
+write.table(data, file = here("Data", "00_raw.txt"), sep = "\t", row.names = FALSE)
 
