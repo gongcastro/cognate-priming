57f623027efcd26289d1d1ad010ac7068ff31510
gongcastro <gonzalo.garciadecastro@upf.edu>
2021-07-23 22:55:19 +0200

    Complete package dependencies in targets

--- _targets.R
+++ _targets.R
@@ -1,6 +1,7 @@
 library(targets)
 library(tarchetypes)
 
+# load functions
 source("R/utils.R")
 source("R/00_stimuli.R")
 source("R/01_participants.R")
@@ -11,7 +12,7 @@ source("R/04_attrition.R")
 source("R/05_prepare.R")
 source("R/06_analysis.R")
 
-# set parameters ----
+# load packages ----
 tar_option_set(
 	packages = c(
 		"dplyr", "tidyr", "stringr", "multilex", "keyring",
@@ -21,27 +22,42 @@ tar_option_set(
 		"rmarkdown", "knitr"
 	)
 )
-options(tidyverse.quiet = TRUE)
 
-# End this file with a list of target objects.
+
+# define targets (see https://books.ropensci.org/targets/)
+# in each target, the value returned by a function (second argument) is assigned to 
+# a variable name (first argument). This object is stored in in the _targets folder
+# and can be imported into the Global environment using tar_load()
+# each target takes some input. Some targets get the result of other targets as inputs
+# you can visualise the dependencies between files, scripts/functions and targets by running tar_visnetwork()
+
+# to execute this script and generate the targets, run tar_make() in your console. This will
+# (1) Skip those targets that are already up to date (nothing they depend on has changed since last time you run tar_make())
+# (2) Run outdated targets (those for which something they depend on has changed since last time)
+# the first time you run, tar_make() may take a while to run entirely
+# it will take shorter after the first time, as it only runs the outdated targets
+
 list(
 	
-	# get multilex data ----
+	# get multilex data (Barcelona vocabulary data) ----
+	# log into multilex
 	tar_target(credentials, get_credentials()),
+	# this returns a list with all necessary data
 	tar_target(multilex_data, get_multilex(update = TRUE, type = "understands")),
 	
 	# stimuli ----
-	# define data
-	tar_target(trials_path, here("Stimuli", "stimuli.xlsx"), format = "file"),
-	tar_target(stimuli_english_path, here("Data", "Stimuli", "stimuli_english.xlsx"), format = "file"),
-	tar_target(animacy_path, here("Data", "Stimuli", "animacy.csv"), format = "file"),
+	# define file paths
+	tar_target(trials_path, here("Stimuli", "stimuli.xlsx"), format = "file"), # trial list,
+	tar_target(stimuli_english_path, here("Data", "Stimuli", "stimuli_english.xlsx"), format = "file"), # stimuli info for English version
+	tar_target(animacy_path, here("Data", "Stimuli", "animacy.csv"), format = "file"), # animacy of stimuli (hand coded)
 	
 	# import data
 	tar_target(trials, read_xlsx(trials_path)),
 	tar_target(stimuli_english, read_xlsx(stimuli_english_path, na = "NA")),
 	tar_target(animacy, read.csv(animacy_path)),
 	
-	# run functions
+	# join all stimuli datasets into a single object
+	# see R/00_stimuli.R for details on this function
 	tar_target(
 		stimuli,
 		get_stimuli(
@@ -53,20 +69,13 @@ list(
 	),
 	
 	# participants ----
-	# define data
+	# define Oxford file paths
 	tar_target(
 		participants_oxf_path,
 		here("Data", "Participants", "participant_oxford_Apr2021.xlsx"),
 		format = "file"
 	),
-	# import data
-	tar_target(
-		participants_bcn,
-		range_read(
-			ss = "1JkhN4iBh3bi6PSReGGk9jSrVgDhZNOUmve6vNS2eEqE",
-			sheet = "barcelona", na = ""
-		)
-	),
+	# import Oxford data
 	tar_target(
 		participants_oxf,
 		read_xlsx(
@@ -74,30 +83,52 @@ list(
 			na = "NA"
 		)
 	),
-	# run functions
+	# import Barcelona data from Google Sheets
+	# in the future, this will be a local CSV
+	tar_target(
+		participants_bcn,
+		range_read(
+			ss = "1JkhN4iBh3bi6PSReGGk9jSrVgDhZNOUmve6vNS2eEqE",
+			sheet = "barcelona", na = ""
+		)
+	),
+	# join datasets
+	# see R/01_participants.R for details on this function
 	tar_target(
 		participants,
 		get_participants(participants_bcn, participants_oxf)
 	),
 	
 	# vocabulary ----
-	# define data
+	# define Oxford file path
 	tar_target(
 		vocabulary_oxf_path,
 		here("Data", "Vocabulary", "vocabulary_oxford_Apr2021.xlsx")
 	),
-	# import data
+	# import Oxford data
 	tar_target(
 		vocabulary_oxf,
 		vocabulary_oxf_path %>% 
+			# get sheets names
 			excel_sheets() %>%
-			map(~read_xlsx(vocabulary_oxf_path, sheet = ., na = c("", "NA", "?", "x"))) %>%
+			# read all sheets into a list
+			map(
+				~read_xlsx(
+					vocabulary_oxf_path,
+					sheet = .,
+					na = c("", "NA", "?", "x")
+				)
+			) %>%
+			# name each sheet
 			set_names(excel_sheets(vocabulary_oxf_path)) %>%
+			# join sheets into a data frame
+			#new variable (version) identifies each sheet
 			bind_rows(.id = "version")
 	),
-	# run functions
+	# join all vocabulary datasets
 	tar_target(
 		vocabulary,
+		# see R/02_vocabulary.R for details on this function
 		get_vocabulary(
 			participants = participants, 
 			update = FALSE,
@@ -108,16 +139,18 @@ list(
 		)
 	),
 	
-	# gaze Barcelona ----
-	# define data
+	# gaze data (Barcelona) ----
+	# define file paths
 	tar_target(
 		gaze_bcn_paths,
 		list.files(
 			here("Data", "Gaze", "Barcelona"),
-			full.names = TRUE)
+			full.names = TRUE
+		)
 	),
 	
-	# run functions
+	# import data
+	# see R/03_gaze_bcn.R and utils.R for details on this function
 	tar_target(
 		gaze_bcn,
 		get_gaze_bcn(
@@ -127,8 +160,8 @@ list(
 		)
 	),
 	
-	# get Oxford ----
-	# define data
+	# gaze data (Oxford) ----
+	# define file paths
 	tar_target(
 		gaze_oxf_paths,
 		list.files(
@@ -136,7 +169,8 @@ list(
 			full.names = TRUE
 		)
 	),
-	# run functions
+	# import data
+	# see R/03_gaze_oxf.R for details on this function
 	tar_target(
 		gaze_oxf,
 		get_gaze_oxf(
@@ -146,7 +180,8 @@ list(
 		)
 	),
 	
-	# get attrition ----
+	# attrition data ----
+	# see R/04_attrition.R for details on this function
 	# stringent version
 	tar_target(
 		attrition,
@@ -155,13 +190,19 @@ list(
 			vocabulary = vocabulary,
 			gaze_bcn = gaze_bcn,
 			gaze_oxf = gaze_oxf,
-			looking_threshold = c(prime = 250, target = 250, distractor = 0), # minimum looking time
-			missing_trials_threshold = c(cognate = 2, noncognate = 2, unrelated = 2), # minimum n trials in each condition
+			# minimum looking time to each picture for valid trial
+			looking_threshold = c(prime = 250, target = 250, distractor = 0), 
+			# minimum number of valid trials in each condition for valid participant
+			missing_trials_threshold = c(cognate = 2, noncognate = 2, unrelated = 2),
+			# what words should the participant know for valid trial?
 			filter_vocabulary = c("prime", "target"),
+			# should target-distractor counterbalancing pairs we filtered out together?
+			# e.g. if TRUE, cat-balloon removed (even if valid) if balloon-cat is not valid
 			filter_counterbalancing = FALSE
 		)
 	),
 	# relaxed version
+	# same function, different argument values (less stringent version)
 	tar_target(
 		attrition_relaxed,
 		get_attrition(
@@ -169,12 +210,13 @@ list(
 			vocabulary = vocabulary,
 			gaze_bcn = gaze_bcn,
 			gaze_oxf = gaze_oxf,
-			looking_threshold = c(prime = 250, target = 250, distractor = 0), # minimum looking time
-			missing_trials_threshold = c(cognate = 0, noncognate = 0, unrelated = 0), # minimum n trials in each condition
+			looking_threshold = c(prime = 250, target = 250, distractor = 0),
+			missing_trials_threshold = c(cognate = 0, noncognate = 0, unrelated = 0),
 			filter_vocabulary = NULL,
 			filter_counterbalancing = FALSE
 		)
 	),
+	# stringent version with filter_counterbalancing = TRUE
 	tar_target(
 		attrition_counter,
 		get_attrition(
@@ -188,6 +230,7 @@ list(
 			filter_vocabulary = c("prime", "target")
 		)
 	),
+	# relaxed version with filter_counterbalancing = TRUE
 	tar_target(
 		attrition_relaxed_counter,
 		get_attrition(
@@ -202,6 +245,9 @@ list(
 		)
 	),
 	# prepare data for analysis ----
+	# see R/05_prepare.R for details on the get_prepared() function
+	# this function returns an analysis ready dataset, with all necessary (transformed/coded) variables
+	# data aggregation and time window definition is performed using the eyetrackingR package
 	# stringent version
 	tar_target(
 		gaze,
@@ -227,30 +273,48 @@ list(
 		)
 	),
 	
-	# fit frequentist models ----
+	# fit models ----
+	# see R/06_analysis.R for details on the fit_models() function
+	# this function takes a list of formulas and list of datasets and fits a model 
+	# that takes each formula-dataset pair at a time, and returns a named list of fits
 	tar_target(
 		model_formulas,
 		list(
+			# this model includes all data and all predictors of interest
 			fit = "elog ~ age_group + vocab_size_l1_center*trial_type*lp*(ot1+ot2) + (1+ot1+ot2+ot3+trial_type+age_group | participant)",
+			# this model includes only data from 21 monolingual participants
 			fit_21_mon = "elog ~  vocab_size_l1_center*trial_type*(ot1+ot2+ot3) + (1+ot1+ot2+ot3+trial_type | participant)",
+			# this model includes only data from 21 monolingual participants and the location predictor (Barcelona vs. Oxford)
 			fit_21_mon_location = "elog ~ trial_type*location*(ot1+ot2+ot3) + (1+ot1+ot2 | participant)",
+			# this model includes only data from 21 monolingual participants and vocabulary in L1 as predictor (median split)
 			fit_21_mon_vocab_split = "elog ~ trial_type*vocab_cat*(ot1+ot2+ot3) + (1+ot1+ot2 | participant)",
+			# this model includes only data from 21 monolingual participants and
+			# only data from the 15 Oxford participants with the lowest vocabulary size,
+			# and 15 Barcelona participants with the highest vocabulary size
+			# we did this because participants in Oxford had higher L1 vocabulary sizes at the time
 			fit_21_mon_vocab_balanced = "elog ~ trial_type*(ot1+ot2+ot3) + (1+ot1+ot2+ot3 | participant)"
 		)
 	),
+	# define the dataset corresponding to each model (same order as in previous target)
+	# stringent inclusion criteria
 	tar_target(
 		model_datasets,
 		list(
 			fit = gaze,
-			fit_21_mon = gaze %>% filter(age_group=="21 months", lp=="Monolingual"),
-			fit_21_mon_location = gaze %>% filter(age_group=="21 months", lp=="Monolingual"),
+			fit_21_mon = gaze %>%
+				filter(age_group=="21 months", lp=="Monolingual"),
+			fit_21_mon_location = gaze %>%
+				filter(age_group=="21 months", lp=="Monolingual"),
 			fit_21_mon_vocab_split = gaze %>%
 				filter(age_group=="21 months", lp=="Monolingual") %>%
-				mutate(vocab_cat = ifelse(
-					vocab_size_l1_center > median(distinct(gaze_relaxed, participant, vocab_size_l1_center)$vocab_size_l1_center, na.rm = TRUE), 
-					"Above median", 
-					"Below median"
-				)) %>% 
+				# vocabulary in L1 median split
+				mutate(
+					vocab_cat = ifelse(
+						vocab_size_l1_center > median(distinct(gaze_relaxed, participant, vocab_size_l1_center)$vocab_size_l1_center, na.rm = TRUE), 
+						"Above median", 
+						"Below median"
+					)
+				) %>% 
 				mutate_at(vars(vocab_cat, location), as.factor),
 			fit_21_mon_vocab_balanced = gaze %>%
 				filter(
@@ -265,6 +329,7 @@ list(
 							group_by(location) %>% 
 							mutate(vocab_index = row_number()) %>% 
 							ungroup() %>% 
+							# get 15 highest in Barcelona and 15 lowest in Oxford
 							filter(
 								((location=="Barcelona") & (vocab_index %in% 1:15)) |
 									((location=="Oxford")) & (vocab_index %in% seq(
@@ -277,15 +342,19 @@ list(
 				)
 		)
 	),
+	# same datasets, but applying the relaxed inclusion criteria
 	tar_target(
 		model_datasets_relaxed,
 		list(
 			fit_relaxed = gaze_relaxed,
-			fit_21_mon_relaxed = gaze_relaxed %>% filter(age_group=="21 months", lp=="Monolingual"),
-			fit_21_mon_location_relaxed = gaze_relaxed %>% filter(age_group=="21 months", lp=="Monolingual"),
+			fit_21_mon_relaxed = gaze_relaxed %>%
+				filter(age_group=="21 months", lp=="Monolingual"),
+			fit_21_mon_location_relaxed = gaze_relaxed %>%
+				filter(age_group=="21 months", lp=="Monolingual"),
 			fit_21_mon_vocab_split_relaxed = gaze_relaxed %>%
 				filter(age_group=="21 months", lp=="Monolingual") %>%
-				mutate(vocab_cat = ifelse(
+				mutate(
+					vocab_cat = ifelse(
 					vocab_size_l1_center > median(distinct(gaze_relaxed, participant, vocab_size_l1_center)$vocab_size_l1_center, na.rm = TRUE), 
 					"Above median", 
 					"Below median"
@@ -317,6 +386,7 @@ list(
 		)
 		
 	),
+	# fit models (stringent criteria)
 	tar_target(
 		model_fits,
 		fit_models(
@@ -324,6 +394,7 @@ list(
 			datasets = model_datasets
 		)
 	),
+	# fit model (relaxed criteria)
 	tar_target(
 		model_fits_relaxed,
 		fit_models(
@@ -331,7 +402,7 @@ list(
 			datasets = model_datasets_relaxed
 		)
 	),
-	# render Rmd
+	# render report.Rmd with the updated model fits
 	tar_render(report, "Rmd/report.Rmd")
 	
 )
